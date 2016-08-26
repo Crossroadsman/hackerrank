c314d61b0e508f4d17f1c15aacd9619e395b1b24
Crossroadsman <crossroadsman@gmail.com>
2016-08-25 22:27:24 -0600

    Update kangaroo.swift

--- algorithms/2_implementation/kangaroo.swift
+++ algorithms/2_implementation/kangaroo.swift
@@ -13,14 +13,37 @@
 // NO
 
 
-// check the trivial cases first
-// if at the outset x1 == x2 return YES
-// if at the outset x1 > x2 AND v1 > v2 (or x2 > x1 AND v2 > v1) return NO
+let inputs = readLine()!.characters.split(" ").map({Int(String($0))!})
+let (x1: Int, v1: Int, x2: Int, v2: Int) = (inputs[0], inputs[1], inputs[2], inputs[3])
 
-// then do math
-x1 + a * v1 == x2 + b * v2 where a and b are positive integers?
+func checkKangaroos() -> String {
 
+  // check the trivial cases first
+  // if at the outset x1 == x2 return YES
+  if x1 == x2 { return "YES" }
+  
+  // if at the outset v1 == v2 return NO (parallel)
+  if v1 == v2 { return "NO" }
 
-// imagine a graph where the jump number is the x-axis and the cumulative distance travelled is the y-axis
-// plot the two kangaroo formulas
-// if the value where the lines intersect is a natural number solution for both equations then YES else NO
+  // if at the outset x1 > x2 AND v1 > v2 (or x2 > x1 AND v2 > v1) return NO
+  if (x1 > x2 && v1 > v2) || (x2 > x1 && v2 > v1) { return "NO" }
+
+  // Math time:
+  // imagine a graph where the jump number is the x-axis and the cumulative distance travelled is the y-axis
+  // plot the two kangaroo formulas
+  // if the value where the lines intersect is a natural number solution for both equations then YES else NO
+  
+  // determine x-intercept by setting same y values
+  let xIntercept = (Double(x2) - Double(x1))/(Double(v1) - Double(v2)) // divide by zero if v1 == v2 (tested above)
+  if xIntercept % 1 != 0 { return "NO" } // not a whole number therefore the x-intercept is between jumps
+  
+  let k1Pos = (v1 * xIntercept) + x1
+  let k2Pos = (v2 * xIntercept) + x2
+  
+  if k1Pos == k2Pos { return "YES" }
+  
+  
+  return "NO"
+}
+
+print(checkKangaroos())
