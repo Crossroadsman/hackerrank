f14c38a6ec8cd3c9c1a901c1e9537679d23dfd34
Crossroadsman <crossroadsman@gmail.com>
2016-08-30 12:46:53 -0600

    Update newYearChaos.swift
    fixed bug (decrementing highestRemainingValue even when the element was not highestRemainingValue). 
    Still causes the tester to time out on tests 6 through 9. Not sure how to improve this algorithm, especially considering some seem to have solved with even slower algorithms.

--- algorithms/2_implementation/newYearChaos.swift
+++ algorithms/2_implementation/newYearChaos.swift
@@ -9,13 +9,16 @@ for testCase in 0.stride(to: testCases, by: 1) {
     var highestRemainingValue = lineLength
     var bribes = 0
     
+    //print("bribes: \(bribes)")
+    
     for i in lineLength.stride(through: 1, by: -1) {
+        //print(" looking at position: \(i), original position: \(line[i - 1])")
         let diff = line[i - 1] - i
         
-        /* debug
-        print("i: \(i), line[i - 1]: \(line[i - 1])")
-        print("therefore diff = \(diff)")
-        */
+        
+        //print("i: \(i), line[i - 1]: \(line[i - 1])")
+        //print("therefore diff = \(diff)")
+        
         
         if diff > 2 {
             isImpossible = true
@@ -28,13 +31,12 @@ for testCase in 0.stride(to: testCases, by: 1) {
         }
         
         for j in (i - 1).stride(through: 1, by: -1) {
+            //print ("  comparing to position (j): \(j) (original position: \(line[j - 1]))")
             if line[j - 1] > line[i - 1] {
                 bribes += 1
+                //print(bribes)
             }
         }
-        
-        
-        highestRemainingValue -= 1
     }
     
     guard !isImpossible else {
