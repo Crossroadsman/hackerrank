e4eee85272fd6bb5901ae0bf6c4c22df9cf9e9d7
Crossroadsman <crossroadsman@gmail.com>
2016-08-30 12:55:02 -0600

    Create maximumDraws.swift

---
+++ mathematics/fundamentals/maximumDraws.swift
@@ -0,0 +1,5 @@
+let numberOfTestCases = Int(String(readLine()!))!
+for i in 0.stride(to: numberOfTestCases, by: 1) {
+    let pairsInDrawer = Int(String(readLine()!))!
+    print(pairsInDrawer + 1)
+}
