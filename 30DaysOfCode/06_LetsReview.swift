98a9ed4360dc9d35af2baae3acab386d29144481
Crossroadsman <crossroadsman@gmail.com>
2016-08-31 09:29:11 -0600

    Create 06_LetsReview.swift

---
+++ 30DaysOfCode/06_LetsReview.swift
@@ -0,0 +1,21 @@
+let testCases = Int(String(readLine()!))!
+
+var testArray = [[Character]]()
+for i in 0.stride(to: testCases, by: 1) {
+    testArray.append([Character](readLine()!.characters))
+}
+
+var evens = [Character]()
+var odds = [Character]()
+
+for i in 0.stride(to: testArray.count, by: 1) {
+    for j in 0.stride(to: testArray[i].count, by: 1) {
+        if j % 2 == 0 { 
+            evens.append(testArray[i][j])
+        } else {
+            odds.append(testArray[i][j])
+        }
+    }
+}
+
+print("\(String(evens)) \(String(odds))")
