0d003d6f78c5900ea9774c2b17081c14f98b6861
Crossroadsman <crossroadsman@gmail.com>
2016-09-08 08:37:03 -0600

    Create conditionalStatements.cpp

---
+++ cpp/introduction/conditionalStatements.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int input;
+    scanf("%d", &input);
+    if (input == 1) {
+        printf("one");
+    } else if (input == 2) {
+        printf("two");
+    } else if (input == 3) {
+        printf("three");
+    } else if (input == 4) {
+        printf("four");
+    } else if (input == 5) {
+        printf("five");
+    } else if (input == 6) {
+        printf("six");
+    } else if (input == 7) {
+        printf("seven");
+    } else if (input == 8) {
+        printf("eight");
+    } else if (input == 9) {
+        printf("nine");
+    } else {
+        printf("Greater than 9");
+    }
+ 
+   return 0;
+}
