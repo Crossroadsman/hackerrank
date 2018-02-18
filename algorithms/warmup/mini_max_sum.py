ce3c913a20f33266669be760326f6997b8128711
Alex Koumparos <crossroadsman@gmail.com>
2018-02-18 15:23:01 -0600

    Update mini_max_sum.py

--- algorithms/warmup/mini_max_sum.py
+++ algorithms/warmup/mini_max_sum.py
@@ -4,22 +4,34 @@ import sys
 import functools
 
 def miniMaxSum(arr):
+    def gt(a, b):
+        '''behaves like > except any value compared to None returns true'''
+        if b == None:
+            return True
+        else:
+            return a > b
+    
+    def lt(a, b):
+        '''behaves like < except any value compared to None returns true'''
+        if b == None:
+            return True
+        else:
+            return a < b
+    
     # Complete this function
-    sorted_arr = sorted(arr)
-    min_value = sorted_arr[0]
-    max_value = sorted_arr[4]
-    rest_arr = arr[1:4]
+    value_so_far = 0
+    min_so_far = None
+    max_so_far = None
+    for value in arr:
+        if gt(value, max_so_far):
+            max_so_far = value
+        if lt(value, min_so_far):
+            min_so_far = value
+        value_so_far += value
     
-    def add(a,b):
-        return a + b
-    
-    sum_rest_arr = functools.reduce(add, rest_arr)
-    min_sum = sum_rest_arr + min_value
-    max_sum = sum_rest_arr + max_value
-    print("{} {}".format(min_sum, max_sum))
+    print("{} {}".format(value_so_far - max_so_far, value_so_far - min_so_far))
     
 
 if __name__ == "__main__":
     arr = list(map(int, input().strip().split(' ')))
     miniMaxSum(arr)
-
