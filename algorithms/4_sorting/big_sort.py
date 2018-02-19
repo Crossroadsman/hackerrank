ebd2cfc239928074cd7b08b1a564d22ef177e07c
Alex Koumparos <crossroadsman@gmail.com>
2018-02-19 15:01:18 -0600

    Update big_sort.py
    Use built-in key=int argument to sorted to avoid costly str->int->str conversion

--- algorithms/4_sorting/big_sort.py
+++ algorithms/4_sorting/big_sort.py
@@ -4,8 +4,14 @@ import sys
 
 def bigSorting(arr):
     # Complete this function
-    nums = [int(x) for x in arr]
-    sorted_nums = sorted(nums)
+    '''turns out that converting str->int (and then implicitly back to str) is very costly, at least in python.
+       the solution is to use the key=<type> argument for sorted which enables python's built-in sorting algorithm
+       to treat the elements as ints without doing a type conversion.
+       
+       There is some nice discussion at:
+       https://developers.google.com/edu/python/sorting
+    '''
+    sorted_nums = sorted(arr, key=int)
     return sorted_nums
 
 if __name__ == "__main__":
